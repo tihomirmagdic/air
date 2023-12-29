196d4172950e9ef6c4c55a68a433b56428e396cb
Tihomir Magdic <tihomir.magdic@gmail.com>
2023-12-29 09:13:57 +0100

    added ctrl+break for win

--- runner/util_windows.go
+++ runner/util_windows.go
@@ -1,17 +1,35 @@
 package runner
 
 import (
+	"fmt"
 	"io"
 	"os/exec"
-	"strconv"
 	"strings"
+	"syscall"
 )
 
 func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
 	pid = cmd.Process.Pid
 	// https://stackoverflow.com/a/44551450
-	kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(pid))
-	return pid, kill.Run()
+	// kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(pid))
+	//return pid, kill.Run()
+	return pid, SendInterrupt(pid)
+}
+
+func SendInterrupt(pid int) error {
+	d, e := syscall.LoadDLL("kernel32.dll")
+	if e != nil {
+		return fmt.Errorf("LoadDLL: %v", e)
+	}
+	p, e := d.FindProc("GenerateConsoleCtrlEvent")
+	if e != nil {
+		return fmt.Errorf("FindProc: %v", e)
+	}
+	r, _, e := p.Call(syscall.CTRL_BREAK_EVENT, uintptr(pid))
+	if r == 0 {
+		return fmt.Errorf("GenerateConsoleCtrlEvent: %v", e)
+	}
+	return nil
 }
 
 func (e *Engine) startCmd(cmd string) (*exec.Cmd, io.ReadCloser, io.ReadCloser, error) {
